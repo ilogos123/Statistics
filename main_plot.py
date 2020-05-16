6c848c92813c356a527820597ce260a684068d31
ilogos <ilogos123@outlook.com>
2020-05-16 11:10:19 +0800

    1. 图形展示：箱型图

--- main_plot.py
+++ main_plot.py
@@ -25,6 +25,19 @@ if __name__ == '__main__':
     # plt.show()
 
     # histogram
-    data = [random.randint(0, 100) for _ in range(100)]
-    plt.hist(data, rwidth=0.8, bins=5, density=True)
+    # data = [random.randint(0, 100) for _ in range(1000)]
+    # plt.hist(data, rwidth=0.8, bins=5, density=True)
+    # plt.show()
+
+    # boxplot
+    # data = [random.randint(0, 100) for _ in range(1000)]
+    # data.append(200)  # 极端值
+    # data.append(-200)  # 极端值
+    # plt.boxplot(data)
+    # plt.show()
+
+    data1 = [random.randint(66, 166) for _ in range(200)]
+    data2 = [random.randint(60, 120) for _ in range(200)]
+    plt.boxplot([data1, data2])
     plt.show()
+
