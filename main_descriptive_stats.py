c60bb0c6f10940aeb39a4a2d416747dc7af0e672
ilogos <ilogos123@outlook.com>
2020-05-16 09:31:23 +0800

    1. 添加中位数计算

--- main_descriptive_stats.py
+++ main_descriptive_stats.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from playStats.descriptive_stats import frequency
 from playStats.descriptive_stats import mode
+from playStats.descriptive_stats import median
 
 if __name__ == '__main__':
     data = [2, 2, 2, 1, 1, 1, 3, 3]
@@ -10,3 +11,8 @@ if __name__ == '__main__':
 
     # 测试众数
     print(mode(data))
+
+    # 测试中位数
+    print(median(data))
+
+
