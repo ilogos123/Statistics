d535c66174f6a5b407630aae37e9f710acb7f8d9
ilogos <ilogos123@outlook.com>
2020-05-16 09:21:33 +0800

    1. 添加众数计算

--- main_descriptive_stats.py
+++ main_descriptive_stats.py
@@ -1,6 +1,12 @@
 from collections import Counter
 from playStats.descriptive_stats import frequency
+from playStats.descriptive_stats import mode
+
 if __name__ == '__main__':
+    data = [2, 2, 2, 1, 1, 1, 3, 3]
+
     # 测试频率
-    data = [2, 2, 2, 2, 1, 1, 1, 3, 3]
     print(frequency(data))
+
+    # 测试众数
+    print(mode(data))
--- playStats/descriptive_stats.py
+++ playStats/descriptive_stats.py
@@ -8,3 +8,21 @@ def frequency(data):
     for point in counter.most_common():
         ret.append((point[0], point[1] / len(data)))
     return ret
+
+
+def mode(data):
+    """寻找众数"""
+    counter = Counter(data)
+    counter_data = counter.most_common()
+
+    if counter_data[0][1] == 1:
+        return None, None
+
+    ret = []
+    count = counter_data[0][1]
+    for point in counter_data:
+        if point[1] == count:
+            ret.append(point[0])
+        else:
+            break
+    return ret, count
