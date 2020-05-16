b4ed5d7b50e0cbd82963f546eddce6204a34db89
ilogos <ilogos123@outlook.com>
2020-05-16 10:28:11 +0800

    1. 图形展示：直方图 rwidth: 条形数据的间隔 bins: 条形数目 density: 是否开启频率直方图，y轴为频率/x轴距离，整个图形的面积为1

--- main_plot.py
+++ main_plot.py
@@ -17,9 +17,14 @@ if __name__ == '__main__':
     # plt.show()
 
     # bar plot
-    data = [3, 3, 4, 1, 5, 4, 2, 1, 5, 4, 4, 4, 5, 3, 2, 1, 4, 5, 5]
-    counter = Counter(data)
-    x = [point[0] for point in counter.most_common()]
-    y = [point[1] for point in counter.most_common()]
-    plt.bar(x, y)
+    # data = [3, 3, 4, 1, 5, 4, 2, 1, 5, 4, 4, 4, 5, 3, 2, 1, 4, 5, 5]
+    # counter = Counter(data)
+    # x = [point[0] for point in counter.most_common()]
+    # y = [point[1] for point in counter.most_common()]
+    # plt.bar(x, y)
+    # plt.show()
+
+    # histogram
+    data = [random.randint(0, 100) for _ in range(100)]
+    plt.hist(data, rwidth=0.8, bins=5, density=True)
     plt.show()
